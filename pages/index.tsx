e15967d021bcee4fa38e0d4cdf92d9b3fa3dcac9
David Wurtz <davidjwurtz@gmail.com>
2022-06-06 14:25:28 -0700

    cleanup

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,35 +1,13 @@
 import Head from 'next/head';
-import { GetStaticProps } from 'next';
-import Post, { PostProps } from '../components/post';
 
-export const getStaticProps: GetStaticProps = async (context) => {
-  // fetch list of posts
-  const response = await fetch(
-    'https://jsonplaceholder.typicode.com/posts?_page=1'
-  );
-  const postList = await response.json();
-  return {
-    props: {
-      postList,
-    },
-  };
-};
-
-export default function IndexPage(props: { postList: PostProps[] }) {
-  const { postList } = props;
+export default function IndexPage() {
   return (
     <main>
       <Head>
-        <title>Home page</title>
+        <title>TERAPHONE</title>
       </Head>
 
-      <h1>List of posts</h1>
-
-      <section>
-        {postList.map((post) => (
-          <Post {...post} key={post.id} />
-        ))}
-      </section>
+      <h1>Hello World</h1>
     </main>
   );
 }
