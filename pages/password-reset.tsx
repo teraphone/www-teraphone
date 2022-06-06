4ec8d3cc07bfeade7c320c1f3e740e99c07e70dd
David Wurtz <davidjwurtz@gmail.com>
2022-06-06 16:22:12 -0700

    placeholder

---
+++ pages/password-reset.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function PasswordReset() {
+  return (
+    <main>
+      <Head>
+        <title>TERAPHONE</title>
+      </Head>
+
+      <h1>Password Reset</h1>
+    </main>
+  );
+}
