541fa374671a41c07ca78a38cf9840807fb91ba2
David Wurtz <davidjwurtz@gmail.com>
2022-06-08 15:44:45 -0700

    minimal pages

---
+++ pages/email-verification.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function EmailVerification() {
+  return (
+    <main>
+      <Head>
+        <title>TERAPHONE</title>
+      </Head>
+
+      <h1>Email Verification</h1>
+    </main>
+  );
+}
---
+++ pages/forgot-password.tsx
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function ForgotPassword() {
+  return (
+    <main>
+      <Head>
+        <title>TERAPHONE</title>
+      </Head>
+
+      <h1>Forgot Password</h1>
+    </main>
+  );
+}
