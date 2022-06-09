eb5de5906bf589132b5ce215a77c922cb6744817
David Wurtz <davidjwurtz@gmail.com>
2022-06-09 16:45:13 -0700

    seems to work

--- pages/email-verification.tsx
+++ pages/email-verification.tsx
@@ -1,27 +1,90 @@
+import { PUBLIC_BASE_URL } from '../util/client';
+import { Box, Container, Grid, TextField, Typography } from '@mui/material';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 export default function EmailVerification() {
   const router = useRouter();
   const { code } = router.query;
+  const [requestStatus, setRequestStatus] = useState<
+    'none' | 'pending' | 'success' | 'error'
+  >('error');
+
+  const handleRequest = useCallback(async () => {
+    setRequestStatus('pending');
+    const url = PUBLIC_BASE_URL + '/email-verification';
+    const body = JSON.stringify({ code });
+    console.log('url:', url);
+    console.log('body:', body);
+    const response = await fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body,
+    });
+    if (response.status === 200) {
+      setRequestStatus('success');
+    } else {
+      setRequestStatus('error');
+    }
+  }, [code]);
 
   useEffect(() => {
-    if (code !== undefined) {
-      console.log('code:', code);
-    } else {
-      console.log('redirecting to /');
-      router.push('/');
+    if (router.isReady) {
+      if (code !== undefined) {
+        console.log('code:', code);
+        handleRequest();
+      } else {
+        console.log('redirecting to /');
+        router.push('/');
+      }
     }
-  }, [code, router]);
+  }, [code, handleRequest, router]);
 
   return (
     <main>
       <Head>
-        <title>TERAPHONE</title>
+        <title>TERAPHONE | Email Verification</title>
       </Head>
 
-      <h1>Email Verification</h1>
+      <Container component="div" maxWidth="xs">
+        <Box
+          sx={{
+            paddingTop: 8,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center',
+          }}
+        >
+          <Typography component="h1" variant="h5">
+            Email Verification
+          </Typography>
+          {(requestStatus === 'none' || requestStatus === 'pending') && (
+            <Box component="div" sx={{ mt: 3 }}>
+              <Typography component="p" variant="body1">
+                Here is your moment of zen.
+              </Typography>
+            </Box>
+          )}
+          {requestStatus === 'success' && (
+            <Box component="div" sx={{ mt: 3 }}>
+              <Typography component="p" variant="body1">
+                Success: thank you for confirming your email.
+              </Typography>
+            </Box>
+          )}
+          {requestStatus === 'error' && (
+            <Box component="div" sx={{ mt: 3 }}>
+              <Typography component="p" variant="body1">
+                Failed: invalid or expired email-verification link. Contact
+                help@teraphone.app for support.
+              </Typography>
+            </Box>
+          )}
+        </Box>
+      </Container>
     </main>
   );
 }
