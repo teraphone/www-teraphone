3eb3070d3cec50ffb314ffce6bf9cde6d78566a6
David Wurtz <davidjwurtz@gmail.com>
2022-06-08 17:20:40 -0700

    redirect if no code

--- pages/password-reset.tsx
+++ pages/password-reset.tsx
@@ -1,6 +1,20 @@
 import Head from 'next/head';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function PasswordReset() {
+  const router = useRouter();
+  const { code } = router.query;
+
+  useEffect(() => {
+    if (code !== undefined) {
+      console.log('code:', code);
+    } else {
+      console.log('redirecting to /');
+      router.push('/');
+    }
+  }, [code]);
+
   return (
     <main>
       <Head>
