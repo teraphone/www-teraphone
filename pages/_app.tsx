9e9585a7dc75414b2360bf9a5657e49025cf8846
David Wurtz <davidjwurtz@gmail.com>
2022-06-09 12:11:44 -0700

    custom app. use mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,23 @@
 import type { AppProps } from 'next/app';
+import { CacheProvider, EmotionCache } from '@emotion/react';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { theme } from '../styles/themes';
+import createEmotionCache from '../styles/createEmotionCache';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+const cache = createEmotionCache();
+
+interface MyAppProps extends AppProps {
+  emotionCache: EmotionCache;
+}
+
+export default function MyApp(props: MyAppProps) {
+  const { Component, pageProps, emotionCache = cache } = props;
+  return (
+    <CacheProvider value={emotionCache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  );
 }
