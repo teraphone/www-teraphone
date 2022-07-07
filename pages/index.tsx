5f9b07821a95e5ad470d9cad6813e51818bfe621
Nathan Ward <nathan.ward@puppet.com>
2022-07-06 22:52:04 -0700

    Move debug links to Sitemap.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import Link from 'next/link';
+import Sitemap from '../components/Sitemap';
 
 const debug = false;
 
@@ -9,37 +9,9 @@ export default function IndexPage() {
       <Head>
         <title>TERAPHONE</title>
       </Head>
+      {debug && <Sitemap />}
 
       <h1>Coming Soon!</h1>
-      {debug && (
-        <>
-          <p>
-            <Link href="/email-verification">
-              <a>/email-verification</a>
-            </Link>
-          </p>
-          <p>
-            <Link href="/email-verification?code=1234">
-              <a>/email-verification?code=1234</a>
-            </Link>
-          </p>
-          <p>
-            <Link href="/password-reset">
-              <a>/password-reset</a>
-            </Link>
-          </p>
-          <p>
-            <Link href="/password-reset?code=1234">
-              <a>/password-reset?code=1234</a>
-            </Link>
-          </p>
-          <p>
-            <Link href="/forgot-password">
-              <a>/forgot-password</a>
-            </Link>
-          </p>
-        </>
-      )}
     </main>
   );
 }
