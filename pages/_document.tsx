e1b6823bdc0339b2b77f7db9dd36075e53e913b5
Nathan Ward <175123+vine77@users.noreply.github.com>
2022-08-10 17:01:15 -0700

    Add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        <link
+          color="#1A91E6"
+          href="/images/teraphone-logo.svg"
+          rel="mask-icon"
+          type="image/svg+xml"
+        />
+        <link
+          href="/images/teraphone-logo.svg"
+          rel="icon"
+          type="image/svg+xml"
+        />
         {/* TODO: Add custom font */}
         {/* <link
           href="https://fonts.googleapis.com/css2?family=Inter&display=optional"
