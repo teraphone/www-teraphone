779f92a8636c354072d63862a8bd941abc007103
David Wurtz <davidjwurtz@gmail.com>
2022-09-01 16:04:16 -0700

    more clean login

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,41 +1,26 @@
 import { useIsAuthenticated, useMsal } from '@azure/msal-react';
 import { InteractionStatus } from '@azure/msal-browser';
 import { loginRequest } from '../src/ms-auth/authConfig';
+import { useEffect } from 'react';
 
 const Login = () => {
   const { instance, inProgress } = useMsal();
   const isAuthenticated = useIsAuthenticated();
 
-  if (isAuthenticated) {
-    console.log('User is authenticated');
-    return null;
-  } else if (
-    inProgress !== InteractionStatus.Startup &&
-    inProgress !== InteractionStatus.HandleRedirect
-  ) {
-    // inProgress check prevents sign-in button from being displayed briefly after returning from a redirect sign-in. Processing the server response takes a render cycle or two
-    instance
-      .handleRedirectPromise()
-      .then((tokenResponse) => {
-        if (!tokenResponse) {
-          const accounts = instance.getAllAccounts();
-          if (accounts.length === 0) {
-            // No user signed in
-            instance.loginRedirect(loginRequest);
-          }
-        } else {
-          // Do something with the tokenResponse
-        }
-      })
-      .catch((err) => {
-        // Handle error
-        console.error(err);
-      });
+  useEffect(() => {
+    if (!isAuthenticated && inProgress === InteractionStatus.None) {
+      instance.loginRedirect(loginRequest).catch(console.error);
+    }
+  }, [inProgress, instance, isAuthenticated]);
 
-    return null;
-  } else {
-    return null;
-  }
+  useEffect(() => {
+    if (isAuthenticated) {
+      const account = instance.getActiveAccount();
+      console.log('user is logged in:', account);
+    }
+  }, [instance, isAuthenticated]);
+
+  return null;
 };
 
 export default Login;
