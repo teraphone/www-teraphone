5937c8d31b671f43fc87d1e26e3dc75420e189f9
David Wurtz <davidjwurtz@gmail.com>
2022-09-02 13:23:22 -0700

    cleanup

--- pages/login.tsx
+++ pages/login.tsx
@@ -1,23 +1,19 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useIsAuthenticated, useMsal } from '@azure/msal-react';
-import { AuthenticationResult, InteractionStatus } from '@azure/msal-browser';
-import { loginRequest, BASE_REDIRECT_URI } from '../src/ms-auth/authConfig';
-import { useEffect, useState } from 'react';
+import { InteractionStatus } from '@azure/msal-browser';
+import { loginRequest, BASE_URI } from '../src/ms-auth/authConfig';
+import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 const Login = () => {
   const { instance, inProgress } = useMsal();
   const isAuthenticated = useIsAuthenticated();
-  const [_authResult, setAuthResult] = useState<AuthenticationResult | null>(
-    null
-  );
   const router = useRouter();
   const { destination, ...query } = router.query;
   const targetPage = destination ? (destination as string) : '/';
   const params = Object.entries(query)
     .map(([key, value]) => `${key}=${value}`)
     .join('&');
-  const targetUrl = BASE_REDIRECT_URI + targetPage + '?' + params;
+  const targetUrl = BASE_URI + targetPage + '?' + params;
 
   useEffect(() => {
     if (inProgress === InteractionStatus.None) {
--- src/ms-auth/authConfig.ts
+++ src/ms-auth/authConfig.ts
@@ -1,21 +1,22 @@
-export const BASE_REDIRECT_URI = 'http://localhost:3000';
-export const BASE_LOGOUT_REDIRECT_URI = 'http://localhost:3000';
+export const BASE_URI = "http://localhost:3000";
+export const REDIRECT_URI = BASE_URI + "/login";
+export const LOGOUT_REDIRECT_URI = BASE_URI;
 
 export const msalConfig = {
     auth: {
         clientId: "9ef60b2f-3246-4390-8e17-a57478e7ec45",
         authority: "https://login.microsoftonline.com/common",
-        redirectUri: BASE_REDIRECT_URI + "/login",
-        postLogoutRedirectUri: BASE_LOGOUT_REDIRECT_URI,
+        redirectUri: REDIRECT_URI,
+        postLogoutRedirectUri: LOGOUT_REDIRECT_URI,
     }
 };
 
 export const setRedirectUri = (destination: string) => {
-    msalConfig.auth.redirectUri = BASE_REDIRECT_URI + destination;
+    msalConfig.auth.redirectUri = BASE_URI + destination;
 }
 
 export const setLogoutRedirectUri = (destination: string) => {
-    msalConfig.auth.postLogoutRedirectUri = BASE_LOGOUT_REDIRECT_URI + destination;
+    msalConfig.auth.postLogoutRedirectUri = BASE_URI + destination;
 }
 
 export const loginRequest = {
