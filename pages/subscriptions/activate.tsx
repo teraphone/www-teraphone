c2042200dd7dff06f2e94d82d6828eec26e4cb61
David Wurtz <davidjwurtz@gmail.com>
2022-09-05 18:09:10 -0700

    Add login redirect with return destination

--- pages/subscriptions/activate.tsx
+++ pages/subscriptions/activate.tsx
@@ -1,7 +1,25 @@
 import { useGetPublicQuery } from '../../src/redux/api';
+import { useIsAuthenticated } from '@azure/msal-react';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const Activate = (): JSX.Element => {
   const { data, error, isLoading } = useGetPublicQuery();
+  const isAuthenticated = useIsAuthenticated();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!isAuthenticated) {
+      const urlObj = {
+        pathname: '/login',
+        query: { destination: '/subscriptions/activate', ...router.query },
+      };
+      console.log('redirecting to:', urlObj);
+      router.push(urlObj).catch(console.error);
+    } else {
+      console.log('is authenticated');
+    }
+  });
 
   if (isLoading) {
     return (
