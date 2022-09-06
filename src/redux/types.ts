1022808986377de9aa6ac700133dd8acd0ee1bb3
David Wurtz <davidjwurtz@gmail.com>
2022-09-05 17:40:14 -0700

    Add auth types

--- src/redux/types.ts
+++ src/redux/types.ts
@@ -7,3 +7,15 @@ export type PeachoneResponse<T> = BasePeachoneResponse & T;
 export type Welcome = {
     "path": string;
 }
+
+export type AuthRequest = {
+    msAccessToken: string;
+}
+
+export type AuthUserInfo = {
+    "oid": string;
+    "tid": string;
+    "email": string;
+    "name": string;
+    "companyName": string;
+}
