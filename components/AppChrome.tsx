7966828bd6a4969f99b1d5ccebd8179477f55517
David Wurtz <davidjwurtz@gmail.com>
2022-09-05 18:47:19 -0700

    Move event callback registration to AppChrome

--- components/AppChrome.tsx
+++ components/AppChrome.tsx
@@ -1,8 +1,37 @@
 import { ReactNode } from 'react';
 import { Box, Container } from '@mui/material';
 import Header from './Header';
+import { useMsal } from '@azure/msal-react';
+import { useEffect, useState } from 'react';
+import {
+  EventType,
+  EventMessage,
+  AuthenticationResult,
+} from '@azure/msal-browser';
 
 const AppChrome = ({ children }: { children: ReactNode }) => {
+  const { instance } = useMsal();
+  const [callbackId, setCallbackId] = useState('');
+
+  useEffect(() => {
+    if (callbackId === '') {
+      const handler = (event: EventMessage) => {
+        if (event.eventType === EventType.LOGIN_SUCCESS && event.payload) {
+          const payload = event.payload as AuthenticationResult;
+          console.log('got payload:', payload);
+          const account = payload.account;
+          instance.setActiveAccount(account);
+        }
+      };
+      const id = instance.addEventCallback(handler) as string;
+      console.log('added event callback:', id);
+      setCallbackId(id);
+      return () => {
+        instance.removeEventCallback(id);
+      };
+    }
+  }, [callbackId, instance]);
+
   return (
     <Box sx={{ overflowX: 'hidden' }}>
       <Header />
