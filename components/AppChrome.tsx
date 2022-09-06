7218fdc450a272a68d02c94d04c9a723b558ccfb
David Wurtz <davidjwurtz@gmail.com>
2022-09-05 19:32:18 -0700

    Undo move event callback registration

--- components/AppChrome.tsx
+++ components/AppChrome.tsx
@@ -1,45 +1,8 @@
 import { ReactNode } from 'react';
 import { Box, Container } from '@mui/material';
 import Header from './Header';
-import { useMsal } from '@azure/msal-react';
-import { useEffect, useState, useCallback } from 'react';
-import {
-  EventType,
-  EventMessage,
-  AuthenticationResult,
-} from '@azure/msal-browser';
-import { useAppDispatch } from '../src/redux/hooks';
-import { setMSAuthResult } from '../src/redux/AuthSlice';
 
 const AppChrome = ({ children }: { children: ReactNode }) => {
-  const dispatch = useAppDispatch();
-  const { instance } = useMsal();
-  const [callbackId, setCallbackId] = useState('');
-
-  const handleEvent = useCallback(
-    (event: EventMessage) => {
-      if (event.eventType === EventType.LOGIN_SUCCESS && event.payload) {
-        const payload = event.payload as AuthenticationResult;
-        console.log('got payload:', payload);
-        dispatch(setMSAuthResult(payload)); // <-- why no work???
-        const account = payload.account;
-        instance.setActiveAccount(account);
-      }
-    },
-    [dispatch, instance]
-  );
-
-  useEffect(() => {
-    if (callbackId === '') {
-      const id = instance.addEventCallback(handleEvent) as string;
-      console.log('added event callback:', id);
-      setCallbackId(id);
-      return () => {
-        instance.removeEventCallback(id);
-      };
-    }
-  }, [callbackId, handleEvent, instance]);
-
   return (
     <Box sx={{ overflowX: 'hidden' }}>
       <Header />
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,13 +5,31 @@ import { globalStyles, theme } from '../styles/themes';
 import AppChrome from '../components/AppChrome';
 import { useRouter } from 'next/router';
 import { MsalProvider } from '@azure/msal-react';
-import { PublicClientApplication } from '@azure/msal-browser';
+import {
+  PublicClientApplication,
+  EventType,
+  EventMessage,
+  AuthenticationResult,
+} from '@azure/msal-browser';
 import { CustomNavigationClient } from '../src/ms-auth/NavigationClient';
 import { msalConfig } from '../src/ms-auth/authConfig';
 import { store } from '../src/redux/store';
 import { Provider } from 'react-redux';
 
 export const msalInstance = new PublicClientApplication(msalConfig);
+
+const accounts = msalInstance.getAllAccounts();
+if (accounts.length > 0) {
+  msalInstance.setActiveAccount(accounts[0]);
+}
+
+msalInstance.addEventCallback((event: EventMessage) => {
+  if (event.eventType === EventType.LOGIN_SUCCESS && event.payload) {
+    const payload = event.payload as AuthenticationResult;
+    const account = payload.account;
+    msalInstance.setActiveAccount(account);
+  }
+});
 
 interface MyAppProps extends AppProps {
   emotionCache: EmotionCache;
