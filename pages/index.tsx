e58f20c3dc87e961523b6a289fb2085bd950f1a4
David Wurtz <davidjwurtz@gmail.com>
2022-10-16 12:50:27 -0700

    factor out form into component

--- pages/index.tsx
+++ pages/index.tsx
@@ -104,12 +104,99 @@ const StyledSection = (props: StyledSectionProps) => {
   );
 };
 
-export default function IndexPage() {
+const EarlyAccessCTA = () => {
   const [submitting, setSubmitting] = useState(false);
   const [error, setError] = useState(false);
   const [validationError, setValidationError] = useState(false);
   const [success, setSuccess] = useState(false);
 
+  return (
+    <form
+      id="signup"
+      noValidate
+      onSubmit={async (event: SyntheticEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        setError(false);
+        setValidationError(false);
+        setSuccess(false);
+
+        const form = event.currentTarget as HTMLFormElement;
+        const isValid = form.checkValidity();
+        if (!isValid) {
+          setValidationError(true);
+          return;
+        }
+
+        const formFields = (event.target as HTMLFormElement).elements;
+        const email = (formFields.namedItem('email') as HTMLInputElement)
+          ?.value;
+
+        if (!email) {
+          setError(true);
+          setSuccess(false);
+          return;
+        }
+
+        try {
+          setSubmitting(true);
+          await fetch('https://api-dev.teraphone.app/v1/public/email-signup', {
+            body: JSON.stringify({ email }),
+            headers: { 'Content-Type': 'application/json' },
+            method: 'POST',
+          });
+          setSuccess(true);
+        } catch (error) {
+          setError(true);
+        }
+        setSubmitting(false);
+      }}
+    >
+      <Grid container justifyContent="left" spacing={1}>
+        <Grid item>
+          <TextField
+            color={success ? 'success' : 'primary'}
+            error={error || validationError}
+            FormHelperTextProps={{
+              sx: {
+                textAlign: 'center',
+                color: success ? 'success.main' : 'text.secondary',
+              },
+            }}
+            fullWidth
+            helperText={
+              (validationError && 'Please enter a valid email address') ||
+              (error && 'There was an error submitting your email') ||
+              (success && 'Thanks for signing up!') ||
+              "We'll never share your email"
+            }
+            InputProps={{ sx: { textAlign: 'center' } }}
+            inputProps={{ pattern: '[^@]+@[^@]+\\.[^@]+' }}
+            label="Email address"
+            name="email"
+            onChange={() => setValidationError(false)}
+            required
+            size="small"
+            sx={{ backgroundColor: 'white', width: '260px' }}
+            type="email"
+            variant="outlined"
+          />
+        </Grid>
+        <Grid item>
+          <Button
+            color="success"
+            disabled={submitting}
+            type="submit"
+            variant="contained"
+          >
+            Request early access
+          </Button>
+        </Grid>
+      </Grid>
+    </form>
+  );
+};
+
+export default function IndexPage() {
   const benefitsSection = {
     headline: 'Welcome to the future of work',
     subheadline:
@@ -290,93 +377,7 @@ export default function IndexPage() {
             collaboration!
           </Typography>
           <Box my={4}>
-            <form
-              id="signup"
-              noValidate
-              onSubmit={async (event: SyntheticEvent<HTMLFormElement>) => {
-                event.preventDefault();
-                setError(false);
-                setValidationError(false);
-                setSuccess(false);
-
-                const form = event.currentTarget as HTMLFormElement;
-                const isValid = form.checkValidity();
-                if (!isValid) {
-                  setValidationError(true);
-                  return;
-                }
-
-                const formFields = (event.target as HTMLFormElement).elements;
-                const email = (
-                  formFields.namedItem('email') as HTMLInputElement
-                )?.value;
-
-                if (!email) {
-                  setError(true);
-                  setSuccess(false);
-                  return;
-                }
-
-                try {
-                  setSubmitting(true);
-                  await fetch(
-                    'https://api-dev.teraphone.app/v1/public/email-signup',
-                    {
-                      body: JSON.stringify({ email }),
-                      headers: { 'Content-Type': 'application/json' },
-                      method: 'POST',
-                    }
-                  );
-                  setSuccess(true);
-                } catch (error) {
-                  setError(true);
-                }
-                setSubmitting(false);
-              }}
-            >
-              <Grid container justifyContent="left" spacing={1}>
-                <Grid item>
-                  <TextField
-                    color={success ? 'success' : 'primary'}
-                    error={error || validationError}
-                    FormHelperTextProps={{
-                      sx: {
-                        textAlign: 'center',
-                        color: success ? 'success.main' : 'text.secondary',
-                      },
-                    }}
-                    fullWidth
-                    helperText={
-                      (validationError &&
-                        'Please enter a valid email address') ||
-                      (error && 'There was an error submitting your email') ||
-                      (success && 'Thanks for signing up!') ||
-                      "We'll never share your email"
-                    }
-                    InputProps={{ sx: { textAlign: 'center' } }}
-                    inputProps={{ pattern: '[^@]+@[^@]+\\.[^@]+' }}
-                    label="Email address"
-                    name="email"
-                    onChange={() => setValidationError(false)}
-                    required
-                    size="small"
-                    sx={{ backgroundColor: 'white', width: '260px' }}
-                    type="email"
-                    variant="outlined"
-                  />
-                </Grid>
-                <Grid item>
-                  <Button
-                    color="success"
-                    disabled={submitting}
-                    type="submit"
-                    variant="contained"
-                  >
-                    Request early access
-                  </Button>
-                </Grid>
-              </Grid>
-            </form>
+            <EarlyAccessCTA />
           </Box>
         </Grid>
         <Grid item xs={12} sm={6} alignSelf="center">
