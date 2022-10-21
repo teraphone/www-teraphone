f3c7442889d40b9499b0becbbc32650806790c72
David Wurtz <davidjwurtz@gmail.com>
2022-10-20 18:20:49 -0700

    adding responsive hooks

--- pages/pricing.tsx
+++ pages/pricing.tsx
@@ -1,6 +1,11 @@
+import { Box, Container, Typography, useMediaQuery } from '@mui/material';
+import { theme } from '../styles/themes';
 import Head from 'next/head';
 
 const Pricing = () => {
+  const sm = useMediaQuery(theme.breakpoints.up('sm'));
+  const md = useMediaQuery(theme.breakpoints.up('md'));
+  const lg = useMediaQuery(theme.breakpoints.up('lg'));
   return (
     <>
       <Head>
@@ -10,6 +15,25 @@ const Pricing = () => {
           content="View subscription and pricing options."
         />
       </Head>
+      <Container>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            textAlign: 'center',
+          }}
+        >
+          <Typography variant="h1" fontSize={64}>
+            Teamwork should be easy.
+          </Typography>
+          <Typography variant="h1" color="primary" fontSize={64}>
+            Start Free.
+          </Typography>
+          <Typography variant="body1">{`is sm: ${sm}`}</Typography>
+          <Typography variant="body1">{`is md: ${md}`}</Typography>
+          <Typography variant="body1">{`is lg: ${lg}`}</Typography>
+        </Box>
+      </Container>
     </>
   );
 };
