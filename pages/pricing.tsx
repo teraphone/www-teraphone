dee390b1416896aef65fbe3a0f0ef6becd855bbd
David Wurtz <davidjwurtz@gmail.com>
2022-10-20 17:03:49 -0700

    create empty page

---
+++ pages/pricing.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+
+const Pricing = () => {
+  return (
+    <>
+      <Head>
+        <title>TERAPHONE | Pricing</title>
+        <meta
+          name="description"
+          content="View subscription and pricing options."
+        />
+      </Head>
+    </>
+  );
+};
+
+export default Pricing;
